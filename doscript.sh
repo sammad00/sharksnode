d68e1ad36ffba15e633c1f429bc387353e579526
Faisal Rehman <faisal.rehman22@hotmail.com>
2021-11-30 15:22:51 +0500

    Update doscript.sh

--- doscript.sh
+++ doscript.sh
@@ -3,4 +3,8 @@ set -x
 
 # This script will be copied on the Deployment server.
 
-docker images
+docker login --username ${{ secrets.DOCKER_USERNAME }} --password ${{ secrets.DOCKER_PASSWORD }}
+
+docker pull frehman/pipe1:v.9.99
+
+docker run -it -d -p 8099:80 --restart unless-stopped --name DO_web frehman/pipe1:v.9.99
