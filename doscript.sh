8c26d8b91830d28d274ca5c1904551d9621b2e65
Faisal Rehman <faisal.rehman22@hotmail.com>
2021-12-01 16:28:31 +0500

    Update doscript.sh

--- doscript.sh
+++ doscript.sh
@@ -5,5 +5,5 @@ set -x
 
 #First of all, we will add the Docker secrets on the deployment server:
 
-sed -i -e 's/DOCKER_USERNAME/'$"{{ secrets.DOCKER_USERNAME }}"'/g' /home/users/frehman/.docker/config.json
-sed -i -e 's/DOCKER_PASSWORD/'$"{{ secrets.DOCKER_PASSWORD }}"'/g' /home/users/frehman/.docker/config.json
+sed -i -e 's/"${{ secrets.DOCKER_USERNAME }}"/''/g' /home/users/frehman/.docker/config.json
+sed -i -e 's/"${{ secrets.DOCKER_PASSWORD }}"/''/g' /home/users/frehman/.docker/config.json
