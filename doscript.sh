732e4d10ea38a30f41e39780317b7270dbd1f954
Faisal Rehman <faisal.rehman22@hotmail.com>
2021-12-02 15:17:27 +0500

    Update doscript.sh

--- doscript.sh
+++ doscript.sh
@@ -4,12 +4,6 @@ set -x
 # This script will be copied on the Deployment server.
 
 #First of all, we will add the Docker secrets on the deployment server:
-
-ssh -i ~/.ssh/deploy.pem -oStrictHostKeyChecking=no frehman@54.91.22.158
-
-scp -i ~/.ssh/deploy.pem -oStrictHostKeyChecking=no doscript.sh frehman@54.91.22.158:/home/users/frehman
-
-ssh -i ~/.ssh/deploy.pem -oStrictHostKeyChecking=no frehman@54.91.22.158 "echo "${{ secrets.DOCKER_PASSWORD }}" | docker login -u "${{ secrets.DOCKER_USERNAME }}" --password-stdin"
        
 docker pull frehman/pipe1:v.9.99
 
