cb131f7738a6d98c44bc9925fcc37c1acb7d9940
Faisal Rehman <faisal.rehman22@hotmail.com>
2024-09-14 21:47:49 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -x
 
 #First of all, we will add the Docker secrets on the deployment server:
        
-docker pull devopsmolvi/webimages:release-1.0
+docker pull devopsmolvi/webimages:${{github.run_number}}
 
-docker run -it -d -p 8080:8080 --restart unless-stopped --name webserver devopsmolvi/webimages:release-1.0
+docker run -it -d -p 8080:8080 --restart unless-stopped --name webserver devopsmolvi/webimages:${{github.run_number}}
 
